2628ad8bb29873893e42197136504b3658034433
leroy <leroy.kariuki@student.moringaschool.com>
2023-07-05 20:25:10 +0300

    done with assignmentS

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
-// Make a request to the API
-
-const axios = require('axios');
-
-const options = {
-  method: 'GET',
-  url: 'https://shoes-collections.p.rapidapi.com/shoes',
-  headers: {
-    'X-RapidAPI-Key': '7458705b19msh6dad1514cd29d33p10ebccjsnc5a163a0332a',
-    'X-RapidAPI-Host': 'shoes-collections.p.rapidapi.com'
+// Function to fetch data from the API
+async function fetchData() {
+    try {
+      const response = await fetch("https://rapidapi.com/kaushiksheel9/api/shoes-collections", {
+        headers: {
+          "X-RapidAPI-Host": "shoes-collections.p.rapidapi.com",
+          "X-RapidAPI-Key": "7458705b19msh6dad1514cd29d33p10ebccjsnc5a163a0332a"
+        }
+      });
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.log("Error fetching data:", error);
+    }
   }
-};
-
-try {
-	const response = await axios.request(options);
-	console.log(response.data);
-} catch (error) {
-	console.error(error);
-}
+  
+  // Function to render shoe collections
+  function renderShoeCollections(shoeCollections) {
+    const container = document.getElementById("shoe-collections");
+    shoeCollections.forEach(shoe => {
+      const shoeElement = document.createElement("div");
+      shoeElement.innerHTML = `
+        <h2>${shoe.name}</h2>
+        <p>${shoe.description}</p>
+        <img src="${shoe.image}" alt="${shoe.name}" />
+      `;
+      container.appendChild(shoeElement);
+    });
+  }
+  
+  // Fetch data from the API and render shoe collections when the page loads
+  window.addEventListener("DOMContentLoaded", async () => {
+    const shoeCollections = await fetchData();
+    renderShoeCollections(shoeCollections);
+  });
+  
