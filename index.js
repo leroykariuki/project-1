24e0299aa8b874e7c18b5ab78a66c7ca38f517f8
leroy <leroy.kariuki@student.moringaschool.com>
2023-07-05 20:56:21 +0300

    done with assignmentS

--- index.js
+++ index.js
@@ -1,36 +1,32 @@
 // Function to fetch data from the API
-async function fetchData() {
+async function GetData() {
     try {
-      const response = await fetch("https://rapidapi.com/kaushiksheel9/api/shoes-collections", {
-        headers: {
-          "X-RapidAPI-Host": "shoes-collections.p.rapidapi.com",
-          "X-RapidAPI-Key": "7458705b19msh6dad1514cd29d33p10ebccjsnc5a163a0332a"
-        }
-      });
+      const response = await Get("https://shoes-collections.p.rapidapi.com/shoes");
       const data = await response.json();
       return data;
     } catch (error) {
-      console.log("Error fetching data:", error);
+      console.log("Error getting data:", error);
     }
   }
   
-  // Function to render shoe collections
-  function renderShoeCollections(shoeCollections) {
-    const container = document.getElementById("shoe-collections");
-    shoeCollections.forEach(shoe => {
-      const shoeElement = document.createElement("div");
-      shoeElement.innerHTML = `
-        <h2>${shoe.name}</h2>
-        <p>${shoe.description}</p>
-        <img src="${shoe.image}" alt="${shoe.name}" />
+  // Function to render the data on the page
+  function renderData(data) {
+    const container = document.getElementById("data-container");
+    data.forEach(item => {
+      const itemElement = document.createElement("div");
+      itemElement.innerHTML = `
+        <h2>${item.title}</h2>
+        <p>${item.description}</p>
+        <p>${item.price}
+        <img src="${item.image}" alt="${item.title}" />
       `;
-      container.appendChild(shoeElement);
+      container.appendChild(itemElement);
     });
   }
   
-  // Fetch data from the API and render shoe collections when the page loads
+  // Fetch data from the API and render it on the page
   window.addEventListener("DOMContentLoaded", async () => {
-    const shoeCollections = await fetchData();
-    renderShoeCollections(shoeCollections);
+    const data = await fetchData();
+    renderData(data);
   });
   
