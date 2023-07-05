c9e8d716afe3a819ea60407b4a656060b65a2519
leroy <leroy.kariuki@student.moringaschool.com>
2023-07-05 20:03:52 +0300

    done with assignmentS

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 // Make a request to the API
-fetch("https://rapidapi.com/kaushiksheel9/api/shoes-collections", {
-  method: "GET",
+
+const axios = require('axios');
+
+const options = {
+  method: 'GET',
+  url: 'https://shoes-collections.p.rapidapi.com/shoes',
   headers: {
-    "X-RapidAPI-Key": "7458705b19msh6dad1514cd29d33p10ebccjsnc5a163a0332a",
-    "Content-Type": "application/json",
-  },
-})
+    'X-RapidAPI-Key': '7458705b19msh6dad1514cd29d33p10ebccjsnc5a163a0332a',
+    'X-RapidAPI-Host': 'shoes-collections.p.rapidapi.com'
+  }
+};
+
+try {
+	const response = await axios.request(options);
+	console.log(response.data);
+} catch (error) {
+	console.error(error);
+}
