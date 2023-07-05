61d92bf66a7e1caf596007f39b052e61041375be
leroy <leroy.kariuki@student.moringaschool.com>
2023-07-05 16:08:26 +0300

    done with assignmentS

---
+++ index.js
@@ -0,0 +1,8 @@
+// Make a request to the API
+fetch("https://rapidapi.com/kaushiksheel9/api/shoes-collections", {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "7458705b19msh6dad1514cd29d33p10ebccjsnc5a163a0332a",
+    "Content-Type": "application/json",
+  },
+})
